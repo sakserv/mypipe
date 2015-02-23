221c9a5d8ed9d13c5d854cac932772255da48f4d
Shane Kumpf <sakserv@sakserv.org>
2015-02-23 07:52:37 -0700

    use the database name as the kafka topic name

--- mypipe-kafka/src/main/scala/mypipe/kafka/KafkaUtil.scala
+++ mypipe-kafka/src/main/scala/mypipe/kafka/KafkaUtil.scala
@@ -8,11 +8,11 @@ object KafkaUtil {
     genericTopic(mutation.table.db, mutation.table.name)
 
   def genericTopic(db: String, table: String) =
-    s"${db}_generic"
+    s"${db}"
 
   def specificTopic(mutation: Mutation[_]): String =
     specificTopic(mutation.table.db, mutation.table.name)
 
   def specificTopic(db: String, table: String): String =
-    s"${db}_specific"
+    s"${db}"
 }
